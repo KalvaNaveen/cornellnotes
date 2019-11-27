65bd677c5c8eb43a4e00a68057a95100bb3133a3
Naveen Kalva <knaveen1403@gmail.com>
2019-11-27 09:33:02 +0530

    Updates

---
+++ precache-manifest.5d0fff6e331b3860219005a72e49a7a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0949158d95170abf47018530b3bc63af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87fe2f6697bc50171607",
+    "url": "/static/css/main.d644d54c.chunk.css"
+  },
+  {
+    "revision": "8d4690ed8feaa053f77a",
+    "url": "/static/js/2.03f6862a.chunk.js"
+  },
+  {
+    "revision": "87fe2f6697bc50171607",
+    "url": "/static/js/main.7136ba57.chunk.js"
+  },
+  {
+    "revision": "8e8e3fb23818c1647171",
+    "url": "/static/js/runtime-main.7c33a759.js"
+  }
+]);
